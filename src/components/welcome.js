d808b025fb784a707982ac27ca868e06ab83ca6a
Justin Edwards <justinedwards1230@gmail.com>
2022-06-20 16:26:33 -0400

    Update welcome.js

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -13,9 +13,17 @@ const WelcomeView = (props) => {
     return (
         <div id="welcome" className="text-center text-bg-light p-3">
             <h5>Welcome to Miles' Driving School</h5>
+            <p>
+                This app is a simple driving simulator.
+                The car is trained to drive around the track by using a reinforcement learning algorithm.
+                You can design a model for the car and tweak various parameters to see how it performs.
+            </p>
+            <p>
+                Warning: This app is still in development. Clearing your browsers LocalStorage tends to fix most bugs.
+            </p>
             <div>
-                <button id="startTrain" onClick = {setTrain} className="btn mx-1 btn-dark">Train</button>
-                <button id="startPlay" onClick = {setPlay} className="btn mx-1 btn-dark">Visualize</button>
+                <button id="startTrain" onClick={setTrain} className="btn mx-1 btn-dark">Train</button>
+                <button id="startPlay" onClick={setPlay} className="btn mx-1 btn-dark">Visualize</button>
             </div>
         </div>
     )
