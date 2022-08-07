311cc8acd33f0c3827a2696693085c5b4517b7dd
Justin Edwards <justinedwards1230@gmail.com>
2022-08-07 01:17:47 -0400

    added basis for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import {
 import Home from "./pages/home";
 import Genetic from "./pages/genetic";
 import Teach from "./pages/teach";
+import { CssBaseline } from "@mui/material";
+import { CssVarsProvider, getInitColorSchemeScript } from "@mui/joy";
+import theme from "./components/theme/theme";
 
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/genetic" element={<Genetic />} />
-				<Route path="/teach" element={<Teach />} />
-			</Routes>
+			<CssVarsProvider defaultMode="system" theme={theme}>
+				<CssBaseline />
+				{getInitColorSchemeScript()}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/genetic" element={<Genetic />} />
+					<Route path="/teach" element={<Teach />} />
+				</Routes>
+			</CssVarsProvider>
 		</BrowserRouter>
 	)
 };
