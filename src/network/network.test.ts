377f298e624c4ed81d6fb0392e39a5f9b15d9d4b
Justin Edwards <justinedwards1230@gmail.com>
2022-08-06 23:46:30 -0400

    Update network.test.ts

--- src/network/network.test.ts
+++ src/network/network.test.ts
@@ -16,58 +16,7 @@ describe('AppConfig', () => {
 })
 
 describe('Network - 1 Neuron', () => {
-	const config = new AppConfig(name, alias);
-	config.sensorCount = 1;
-	config.layers = [{
-		id: 0,
-		activation: "Linear",
-		inputs: 1,
-		outputs: 1,
-		lr: 0.01,
-		biases: [0],
-		weights: [[0.5]]
-	}];
-
-	it('load network', () => {
-		const network = new Network(config);
-		expect(network.name).toBe(name);
-		expect(network.alias).toBe(alias);
-		expect(network.layers.length).toBeGreaterThan(0);
-	})
-
-	it('train loop', () => {
-		const network = new Network(config);
-
-		// output = weight * input + bias
-		// y = 0.5x + 0
-		// expected output = [0.5]
-		const input = [1];
-		const output = network.forward(input, true);
-		expect(output).toBeDefined();
-		expect(output).not.toBeNull();
-		expect(output.length).toBe(1);
-		expect(output[0]).toBe(0.5);
-
-		// find error gradient
-		const expected = [0.5];
-		const error = network.lossFunction(expected, output);
-		expect(error).toBe(0);
-		const gradient = network.deriveLoss(expected, output);
-		expect(gradient.length).toBe(1);
-		expect(gradient[0]).toBe(0);
-
-		// update weights and biases
-		// no error means no change
-		// should be the same as the original config
-		network.backward(gradient);
-		const layer = network.layers[0];
-		expect(layer.weights[0][0]).toBe(config.layers[0].weights![0][0]);
-		expect(layer.biases[0]).toBe(config.layers[0].biases![0]);
-	})
-})
-
-describe('Layer - 1 Neuron', () => {
-	const config = {
+	const layerConfig = {
 		activation: 'Linear',
 		inputs: 1,
 		outputs: 1,
@@ -76,24 +25,67 @@ describe('Layer - 1 Neuron', () => {
 		biases: [0],
 		weights: [[0.5]],
 	}
+	const config = new AppConfig(name, alias);
+	config.sensorCount = 1;
+	config.layers = [layerConfig];
 
-
-	it('load layer', () => {
-		const layer = new Linear(config);
-		expect(layer.biases.length).toBe(config.biases.length);
-		expect(layer.weights.length).toBe(config.weights.length);
+	it('load network', () => {
+		const network = new Network(config);
+		expect(network.name).toBe(name);
+		expect(network.alias).toBe(alias);
+		expect(network.layers.length).toBeGreaterThan(0);
 	})
 
-	// output = weight * input + bias
-	// y = 0.5x + 0
-	// expected output = [0.5]
-	const input = [1];
-	it('forward pass', () => {
-		const layer = new Linear(config);
-		const output = layer.forward(input);
-		expect(output).toBeDefined();
-		expect(output).not.toBeNull();
-		expect(output.length).toBe(1);
-		expect(output[0]).toBe(0.5);
+	describe('Layer - Linear', () => {
+		layerConfig.activation = 'Linear';
+		
+		it('load layer', () => {
+			const layer = new Linear(layerConfig);
+			expect(layer.biases.length).toBe(layerConfig.biases.length);
+			expect(layer.weights.length).toBe(layerConfig.weights.length);
+		})
+	
+		// output = weight * input + bias
+		// y = 0.5x + 0
+		// expected output = [0.5]
+		const input = [1];
+		it('forward pass', () => {
+			const layer = new Linear(layerConfig);
+			const output = layer.forward(input);
+			expect(output).toBeDefined();
+			expect(output).not.toBeNull();
+			expect(output.length).toBe(1);
+			expect(output[0]).toBe(0.5);
+		})
+
+		it('train loop', () => {
+			const network = new Network(config);
+	
+			// output = weight * input + bias
+			// y = 0.5x + 0
+			// expected output = [0.5]
+			const input = [1];
+			const output = network.forward(input, true);
+			expect(output).toBeDefined();
+			expect(output).not.toBeNull();
+			expect(output.length).toBe(1);
+			expect(output[0]).toBe(0.5);
+	
+			// find error gradient
+			const expected = [0.5];
+			const error = network.lossFunction(expected, output);
+			expect(error).toBe(0);
+			const gradient = network.deriveLoss(expected, output);
+			expect(gradient.length).toBe(1);
+			expect(gradient[0]).toBe(0);
+	
+			// update weights and biases
+			// no error means no change
+			// should be the same as the original config
+			network.backward(gradient);
+			const layer = network.layers[0];
+			expect(layer.weights[0][0]).toBe(config.layers[0].weights![0][0]);
+			expect(layer.biases[0]).toBe(config.layers[0].biases![0]);
+		})
 	})
 })
